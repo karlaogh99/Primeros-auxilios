141da11e05c0649cecae56e8faef68a86f6bc65f
Carlos García Hernández <carlosgarciahernandez@gmail.com>
2023-01-17 21:04:58 +0000

    Estilo cambiado

--- FirstAid/components/pages/ReaccionAlergica.jsx
+++ FirstAid/components/pages/ReaccionAlergica.jsx
@@ -17,9 +17,11 @@ const ReaccionAlergica = () => {
   return (
     <View style={styles.principal}>
       <ScrollView style={styles.central}>
-        <Text style={styles.letras1}>
+        <View style={styles.cajaTitulos}>
+        <Text style={styles.letrasTitulos}>
           {t("reaccionalergica2")} 
         </Text>
+        </View>
         <Text style={styles.letras1}>
           {t("reaccionalergica3")}
         </Text>
@@ -44,11 +46,15 @@ const ReaccionAlergica = () => {
         <Text style={styles.letras1}>
           {t("reaccionalergica10")}
         </Text>
-        <View>
-        <Text>{t("quehacer")}</Text>
-            <Text style={styles.letras1}>
+        <View style={styles.cajaTitulos}>
+          <Text style={styles.letrasTitulos}>{t("quehacer")}</Text>
+        </View>
+        <View style={styles.cajaSecundarios}>
+        <Text style={styles.letras1}>
           {t("reaccionalergica11")}
         </Text>
+        </View>
+        
         <Text style={styles.letras1}>
           {t("reaccionalergica12")}
         </Text>
@@ -64,8 +70,34 @@ const ReaccionAlergica = () => {
         <Text style={styles.letras1}>
           {t("reaccionalergica16")}
         </Text>
-        
+        <View style={styles.cajaSecundarios}>
+        <Text style={styles.letras1}>
+          {t("reaccionalergica17")}
+        </Text>
         </View>
+        
+        <Text style={styles.letras1}>
+          {t("reaccionalergica18")} <TouchableOpacity style={{backgroundColor:'white'}}><Text style={{}}> {t("primeroenLlegar1")}</Text></TouchableOpacity>
+        </Text>
+        <Text style={styles.letras1}>
+          {t("reaccionalergica19")}
+        </Text>
+        <Text style={styles.letras1}>
+          {t("reaccionalergica20")}
+        </Text> 
+        <Text style={styles.letras1}>
+          {t("reaccionalergica21")}
+        </Text>
+        <View style={styles.cajaSecundarios}>
+        <Text style={styles.letras1}>
+          {t("reaccionalergica22")}
+        </Text>
+        </View>
+        
+        <Text style={styles.letras1}>
+          {t("reaccionalergica23")}
+        </Text>
+
         
       </ScrollView>
     </View>
@@ -80,8 +112,30 @@ const styles = StyleSheet.create({
 
 
   },
-  central:{
+  cajaTitulos:{
+    width:'100%',
+    height:30,
+    alignItems: 'center',
+    justifyContent: 'center',
     
+    backgroundColor:'red',
+    borderRadius: 30,
+  },
+  cajaSecundarios:{
+    width:'100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    
+    backgroundColor:'#0197A9',
+    borderRadius: 10,
+  },
+  letrasTitulos:{
+    fontSize:23,
+    color:'white' ,
+
+  },
+  central:{
+    margin:15,
     width:'90%',
     backgroundColor:'#ffa002',
   },
